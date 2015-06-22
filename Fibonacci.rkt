a79b6a5785959e282efcff3e5b952b688684ac27
rafaelreia <rafael.reia@gmail.com>
2015-06-22 17:30:56 +0100

    presentation improved, a lot, really, u rock

--- Fibonacci.rkt
+++ Fibonacci.rkt
@@ -10,30 +10,47 @@
                      (loop current
                            (+ previous current)
                            (+ index 1)))))))
-    (for ([fib (in-list fibs)])
+    
+    (for ((fib (in-list fibs)))
       (displayln fib))))
 
 
-
+(define (compute-fibonacci n)
+  (let loop ((previous 0)
+             (current 1)
+             (index 0))
+    (if (= index n)
+        (list)
+        (cons current
+              (loop current
+                    (+ previous current)
+                    (+ index 1))))))
+(define (print-list fibs)
+  (for ((fib (in-list fibs)))
+    (displayln fib)))
+(define (fibsR n)
+  (let ((fibs
+         (compute-fibonacci n)))
+    (print-list fibs)))
 
 
 
 
 #;#;(define (fib n)
-  (define fib_numbers (list))
-  (let ((previous 0)
-        (current 1)
-        (index 0))
-    (for ([index (in-range n)])
-      ;(displayln index)
+      (define fib_numbers (list))
+      (let ((previous 0)
+            (current 1)
+            (index 0))
+        (for ([index (in-range n)])
+          ;(displayln index)
           (set! fib_numbers (append fib_numbers (list (+ current previous))))
           (set! previous current)
           (set! current (+ current previous))
           )
-    (for ([fib (in-list fib_numbers)])
-      (displayln fib)
-      )
-    ))
+        (for ([fib (in-list fib_numbers)])
+          (displayln fib)
+          )
+        ))
 
 (define (append-element lst elem)
   (foldr cons (list elem) lst))
