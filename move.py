a4d000fe0975d5fdbbdc4fa0ef13b2e8877e97f5
Rafael Reia <rafael.reia@gmail.com>
2015-07-13 19:51:02 +0100

    move updatedgit add move.py

--- move.py
+++ move.py
@@ -1,6 +1,8 @@
 import shutil
 print "Copy from gitHub to workPlace"
 shutil.copyfile("/home/rafael/Documents/tese/gui.rkt", "/home/rafael/share/racket/pkgs/drracket/drracket/private/syncheck/gui.rkt")
+shutil.copyfile("/home/rafael/Documents/tese/code-walker.rkt", "/home/rafael/share/racket/pkgs/drracket/drracket/private/syncheck/code-walker.rkt")
+shutil.copyfile("/home/rafael/Documents/tese/online-comp.rkt", "/home/rafael/share/racket/pkgs/drracket/drracket/private/syncheck/online-comp.rkt")
 
 #shutil.copyfile("/home/rafael/Documents/tese/aux.txt", "/home/rafael/share/racket/pkgs/drracket/drracket/private/syncheck/aux.txt")
 
