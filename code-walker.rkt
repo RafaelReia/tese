870caad0688a5f699ce8822b40cce81f3638f8d7
rafaelreia <rafael.reia@gmail.com>
2015-07-13 17:40:48 +0100

    code-walker improved

--- code-walker.rkt
+++ code-walker.rkt
@@ -4,23 +4,63 @@
 
 (define (code-walker code)
   
-  (define program-list (syntax-e code))
-  (displayln program-list)
-  (for ((sexp (in-list program-list)))
-    (displayln sexp))
+  ;(define program-structure (syntax-e code))
+  #;(define program-aux program-structure)
+  (define final-list null)
+  (displayln 
+   (let extract-syntax ((program-structure (syntax-e code)))
+     (display "ENTREI, null? ")
+     (displayln (null? program-structure))
+     (unless (null? program-structure)
+       (let loop ((program-aux program-structure)
+                  (current (car program-structure))
+                  (next (cdr program-structure)))
+         (if (null? next)
+             (list)
+             (begin 
+               (displayln current)
+               (display "PAIR? ")
+               (displayln (pair? (cdr program-aux)))
+               ;(displayln (cdr program-aux))
+               (display "Syntax? ")
+               (displayln (syntax? (cdr program-aux)))
+               (displayln " END ITERATION ")
+               (if (syntax? next)
+                   (begin
+                     ;(displayln (vector? current)) ;#f
+                     ;(displayln (box? current)) ;#f
+                     ;(displayln (syntax? current)) #t
+                     ;(displayln (syntax-e current))
+                     (displayln " SYNTAX ")
+                     (display "DATUM ")
+                     (displayln (syntax->datum current))
+                     (extract-syntax (syntax-e current))
+                     #;(when (null? (syntax-e next))
+                         (begin 
+                           (display "DATUM ")
+                           (displayln (syntax->datum current))
+                           (extract-syntax (syntax-e current)))))
+                   (begin
+                     (displayln "NEXT PAIR")
+                     (cons current
+                           (loop (cdr program-aux)
+                                 (cadr program-aux)
+                                 (cddr program-aux)))))))))))
+  (displayln "END FILE")
+  
+  
+  #|(displayln  program-structure)
+  #;(for ((sexp (in-list  program-structure)))
+      (displayln sexp))
   #;(displayln (syntax? code))
   #;(displayln (identifier? code))
   #;(displayln (syntax-source code))
   #;(displayln (syntax-line code))
   #;(displayln (syntax-column code))
   
-   #;(displayln code) ;in theory this is the syntax object of the program
-;test One layer
+  #;(displayln code) ;in theory this is the syntax object of the program
+  ;test One layer
   #;(displayln (syntax-e code))
   ;test EVERYTHING
-  #;(displayln (syntax->datum code))
-  ;Test list, readable information
-  #;(define testlist (syntax->list code))
-  #;(for ((sexp (in-list testlist)))
-      (displayln sexp)))
- 
+  #;(displayln (syntax->datum code)) |#
+  )
