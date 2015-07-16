2a537963ba137765059e30dd14bd17058a4ee237
rafaelreia <rafael.reia@gmail.com>
2015-07-16 16:45:42 +0100

    code-walker refactored and improved

--- code-walker.rkt
+++ code-walker.rkt
@@ -8,36 +8,91 @@
   ;(define program-structure (syntax-e code))
   #;(define program-aux program-structure)
   (define final-list null)
-  (define deep #f)
-  (displayln 
-   (let extract-syntax ((program-structure (syntax-e code))) ;goes down 1 level
-     (display "ENTREI, null? ")
-     (displayln (null? program-structure))
-     (unless (null? program-structure)
-       (let loop ((program-aux program-structure) ;walks trought a level.
-                  (current (car program-structure))
-                  (next (cdr program-structure)))
-         (if (null? next)
-             (list)
+  (define (syntax-walker program current next previous)
+    (displayln "Syntax-walker in the HOUSE!!")
+    (let loop ((program program)
+               (current current)
+               (next next)
+               (previous previous))
+      (display "[Syntax-walker]  Current: ")
+      (displayln current)
+      (display "[Syntax-walker]  PAIR? ")
+      (displayln (pair? next))
+      (display "[Syntax-walker]  Syntax Next? ")
+      (displayln (syntax? next))
+      (cond
+        [(pair? next) (loop (car program) (cadr program) (cddr program) current)]
+        [(and (syntax? next) (not (null? (syntax-e next)))) (loop (syntax-e next) (car (syntax-e next)) (cdr (syntax-e next)) current)]
+        [else (displayln "$$$$$$$$$$$$$$$ Case not supported, aka You found a BUG!! $$$$$$$$$$$$$$$")]))
+    #;(displayln (syntax->datum program)))
+  
+  (define (walk-trought program current next previous)
+    (if (null? next)
+        (list)
+        (begin
+          (display "Current: ")
+          (displayln current)
+          (display "PAIR? ")
+          (displayln (pair? next))
+          ;(displayln (cdr program-aux))
+          (display "Syntax? ")
+          (displayln (syntax? next))
+          (cond
+            [(syntax? next) ;Found syntax, go to there.
              (begin
-               (display "Current: ")
-               (displayln current)
-               (display "PAIR? ")
-               (displayln (pair? next))
-               ;(displayln (cdr program-aux))
-               (display "Syntax? ")
-               (displayln (syntax? next))
-               (displayln " END ITERATION ")
-               (display "Check if current is syntax: ")
-               (displayln (syntax? current))
-
-               (when (and (syntax? next) (syntax? current) deep) ;it has to be the next, only carrying about the deep flag. #FIXTHIS
-                 (begin
-                   (display "DEEP ENOUGH!?!          ")
-                   (displayln next)
-                   (extract-syntax (syntax-e next))
+               (displayln " SYNTAX ")
+               (display "DATUM ")
+               (displayln (syntax->datum current))
+               ;(go-to-syntax (syntax-e current))
+               (define aux (syntax-e current))
+               (displayln (syntax->datum current))
+               (syntax-walker aux (car aux) (cdr aux) null )
+               )]
+            [else 
+             (begin
+               (displayln "NEXT PAIR")
+               (cons current
+                     (walk-trought (cdr program)
+                                   (cadr program)
+                                   (cddr program) 
+                                   null)))]))))
+  (define (go-to-syntax program-structure)
+    (display "[go-to-syntax] program-structure null? ")
+    (displayln (null? program-structure))
+    (unless (null? program-structure)
+      (walk-trought program-structure (car program-structure) (cdr program-structure) null ))
+    (displayln "!!!!!!!!!!!! END OF FILE !!!!!!!!!!!!")
+    )
+  (go-to-syntax (syntax-e code))
+  #;(displayln 
+     (let extract-syntax ((program-structure (syntax-e code))) ;goes down 1 level
+       (display "ENTREI, null? ")
+       (displayln (null? program-structure))
+       (unless (null? program-structure)
+         (let loop ((program-aux program-structure) ;walks trought a level.
+                    (current (car program-structure))
+                    (next (cdr program-structure)))
+           (if (null? next)
+               (list)
+               (begin
+                 (display "Current: ")
+                 (displayln current)
+                 (display "PAIR? ")
+                 (displayln (pair? next))
+                 ;(displayln (cdr program-aux))
+                 (display "Syntax? ")
+                 (displayln (syntax? next))
+                 (displayln " END ITERATION ")
+                 (display "Check if current is syntax: ")
+                 (displayln (syntax? current))
+                 
+                 (when (and (syntax? next) (syntax? current) deep) ;it has to be the next, only carrying about the deep flag. #FIXTHIS
+                   (begin
+                     (display "DEEP ENOUGH!?!          ")
+                     (displayln next)
+                     (extract-syntax (syntax-e next))
+                     )
                    )
-                 )
                  (if (syntax? next) 
                      (begin
                        (set! deep #t)
@@ -51,10 +106,10 @@
                              (loop (cdr program-aux)
                                    (cadr program-aux)
                                    (cddr program-aux)))))))))))
-   (displayln "END FILE")
-   
-   
-   #|(displayln  program-structure)
+  (displayln "END FILE")
+  
+  
+  #|(displayln  program-structure)
   #;(for ((sexp (in-list  program-structure)))
       (displayln sexp))
   #;(displayln (syntax? code))
@@ -68,5 +123,4 @@
   #;(displayln (syntax-e code))
   ;test EVERYTHING
   #;(displayln (syntax->datum code)) |#
-   )
-  
+  )
